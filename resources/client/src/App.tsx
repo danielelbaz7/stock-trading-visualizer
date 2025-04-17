530d5d9d65e68d8caf659fe38a33dd1f8d917b33
danielelbaz7 <175679070+danielelbaz7@users.noreply.github.com>
2025-04-16 21:26:43 -0400

    backend to frontend connection is working

--- resources/client/src/App.tsx
+++ resources/client/src/App.tsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 import {
   LineChart,
   Line,
@@ -9,17 +11,32 @@ import {
   ResponsiveContainer
 } from 'recharts';
 
-const data = [
-  { date: 'Jan', close: 400},
-];
+
 
 function App() {
+
+  const [prices, setPrices] = useState([{}]);
+
+  const getPricesInJson = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/data/GOOG/2015-01-01/2024-01-01/');
+      if (!response.ok) {
+        console.log(response);
+        throw new Error('Prices could not be fetched!');
+      }
+      const jsonPrices = await response.json();
+      setPrices(jsonPrices);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div style={{ width: 400, height: 400 }}>
       <h2>Historical Stock Price (USD)</h2>
       <ResponsiveContainer>
         <LineChart
-          data={data}
+          data={prices}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
           <CartesianGrid strokeDasharray="3 3" />
@@ -35,6 +52,7 @@ function App() {
           />
         </LineChart>
       </ResponsiveContainer>
+      <button onClick={getPricesInJson}>Get Prices</button>
     </div>
   );
 }
--- resources/server/server.py
+++ resources/server/server.py
@@ -6,17 +6,18 @@ app.config['JSON_SORT_KEYS'] = False
 CORS(app)
 
 # returns ticket data from start date to end date as a json file to be parsed
-@app.route('/data/<ticker>/<start_date>/<end_date>', methods=['GET'])
+@app.route('/data/<ticker>/<start_date>/<end_date>/', methods=['GET'])
 def get_data(ticker, start_date, end_date):
 
     DataFrameReturner.load_data(ticker, start_date=start_date, end_date=end_date)
     data = DataFrameReturner.get_dataframe()[['Close']]
 
-    price_dict = {}
-    for index, row in data.iterrows():
-        price_dict[index.strftime("%Y-%m-%d")] = float(row['Close'])
+    price_list = [
+        {"date": index.strftime("%Y-%m-%d"), "close": float(row['Close'])}
+        for index, row in data.iterrows()
+    ]
 
-    return jsonify(price_dict)
+    return jsonify(price_list)
 
 if __name__ == '__main__':
-    print("Run")
+    app.run(debug=True)
