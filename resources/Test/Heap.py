01987b51668295874dbb164f34215d99eb95a1b2
mattbeutel <matthewbeutel@ufl.edu>
2025-04-16 20:24:58 -0400

    Implemented heap push and heapify methods

--- resources/Test/Heap.py
+++ resources/Test/Heap.py
@@ -2,3 +2,16 @@ class MinHeap:
     def __init__(self):
         self.prices = []
         self.actualSize = 0 #Necessary due to lazy deletion provess fcr sliding window
+
+    def heapifyUp(self, index):
+        while index > 0:
+            #heapifies up until either the price is in a valid position, or the price is the root of the heap
+            if self.prices[index] >= self.prices[(index-1)//2]:
+                break
+            self.prices[index], self.prices[(index-1)//2] = self.prices[(index-1)//2], self.prices[index]
+            index = (index-1)//2
+
+    def heapPush(self, price):
+        self.prices.append(price)
+        self.heapifyUp(self.actualSize)
+        self.actualSize += 1
