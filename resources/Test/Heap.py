4e5b5a496ac2b99fd0056e6b4818e13712225d5d
mattbeutel <matthewbeutel@ufl.edu>
2025-04-16 20:41:08 -0400

    fixed heappush heapify indexing, implemented heapPop and heapifyDown

--- resources/Test/Heap.py
+++ resources/Test/Heap.py
@@ -1,7 +1,7 @@
 class MinHeap:
     def __init__(self):
         self.prices = []
-        self.actualSize = 0 #Necessary due to lazy deletion provess fcr sliding window
+        self.actualSize = 0 #Necessary due to lazy deletion process fcr sliding window
 
     def heapifyUp(self, index):
         while index > 0:
@@ -13,5 +13,25 @@ class MinHeap:
 
     def heapPush(self, price):
         self.prices.append(price)
-        self.heapifyUp(self.actualSize)
+        self.heapifyUp(len(self.prices) - 1)
         self.actualSize += 1
+
+    def heapifyDown(self, index):
+        while index < len(self.prices)//2:
+            if self.prices[index] > self.prices[(index+1)*2 - 1]:
+                if len(self.prices) >= (index+1)*2 and self.prices[(index+1)*2] < self.prices[(index+1)*2 - 1]:
+                    self.prices[index], self.prices[(index+1)*2] = self.prices[(index+1)*2], self.prices[index]
+                    index = (index+1)*2
+                else:
+                    self.prices[index], self.prices[(index+1)*2-1] = self.prices[(index+1)*2 - 1], self.prices[index]
+                    index = (index+1)*2 - 1
+            elif len(self.prices) >= (index+1)*2 and self.prices[index] > self.prices[(index+1)*2]:
+                self.prices[index], self.prices[(index+1)*2] = self.prices[(index+1)*2], self.prices[index]
+                index = (index+1)*2
+            else:
+                break
+
+    def heapPop(self):
+        self.prices[0], self.prices[len(self.prices) - 1] = self.prices[len(self.prices) - 1], self.prices[0]
+        self.prices.pop()
+        self.heapifyDown(0)
