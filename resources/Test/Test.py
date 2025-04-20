5fbc5a5f194d43b43b4e3867ea3c7c8629ee4a1d
bakarikerr <bakarikerr@ufl.edu>
2025-04-20 15:00:42 -0400

    Changed to fit new encapsulated functionality

--- resources/Test/Test.py
+++ resources/Test/Test.py
@@ -1,7 +1,23 @@
-import yfinance as yf
-from backtesting import Backtest
-from resources.server.Helper import modify_data
-from Strategies import FirstStrategy, SecondStrategy
+# Copy this class into server to test ya code
+'''
+from toolkit import DataFrameReturner
+from TradingAlgorithms import MedianAlgorithm
+from backtesting import Strategy
+from backtesting.lib import crossover
+from backtesting.test import SMA
+
+class DummyStrategy(Strategy):
+    # Placeholder strategy
+    def init(self):
+        price = self.data.Close
+        self.ma1 = self.I(SMA, price, 10)
+        self.ma2 = self.I(SMA, price, 20)
+
+    def next(self):
+        if crossover(self.ma1, self.ma2):
+            self.buy()
+        elif crossover(self.ma2, self.ma1):
+            self.sell()
 
 def main():
     print("Enter stock ticker symbol: ", end="")
@@ -11,12 +27,10 @@ def main():
     print("Enter end date (XXXX-XX-XX): ", end="")
     end_date = input()
 
-    raw_data = yf.download(ticker, start=start_date, end=end_date)
-    modified_data = modify_data(raw_data)
-
-    btpy = Backtest(modified_data, FirstStrategy,
-              exclusive_orders=True)
-    stats = btpy.run()
+    DataFrameReturner.load_data(ticker, start_date, end_date)
+    # Change this to any other strategy you wanna test
+    stats = DataFrameReturner.run_btpy(MedianAlgorithm)
+    DataFrameReturner.plot()
     trades = stats['_trades']
     print(stats)
     print()
@@ -24,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
